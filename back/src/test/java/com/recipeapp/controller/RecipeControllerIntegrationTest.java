930d696bdd79247198e3fc6fce7d661c3fe1079b
guidon <V-GUIDON-externe@efluid.fr>
2025-12-23 14:04:53 +0100

    fix(test): add Sring context runner

--- back/src/test/java/com/recipeapp/controller/RecipeControllerIntegrationTest.java
+++ back/src/test/java/com/recipeapp/controller/RecipeControllerIntegrationTest.java
@@ -7,8 +7,12 @@ import com.recipeapp.dto.RecipeRequest;
 import com.recipeapp.entity.Recipe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
@@ -16,6 +20,10 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 public class RecipeControllerIntegrationTest extends BaseIntegrationTest {
     
     private String authToken;
